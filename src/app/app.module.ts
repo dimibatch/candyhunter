ebc78829c28229c43230e25796308d54441edead
Dimitri Batchevitch <dimitri.batchevitch@gmail.com>
2018-10-30 19:11:37 +0100

    ajout html et css liste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListeBonbonsComponent } from './components/liste-bonbons/liste-bonbons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeBonbonsComponent
   ],
   imports: [
     BrowserModule
