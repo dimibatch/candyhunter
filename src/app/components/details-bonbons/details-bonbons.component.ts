5435d19cba942377198385bd071e953414247f68
Madi-Naf <onplayer6@gmail.com>
2018-10-31 05:44:43 +0100

    location class created

--- src/app/components/details-bonbons/details-bonbons.component.ts
+++ src/app/components/details-bonbons/details-bonbons.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ApiDetailsService } from '../../services/api-details.service';
 import { Details } from '../../models/details';
 
+import {Location} from 'src/app/models/location';
+import { LocationService} from 'src/app/services/location.service';
+
 @Component({
   selector: 'app-details-bonbons',
   templateUrl: './details-bonbons.component.html',
@@ -9,13 +12,21 @@ import { Details } from '../../models/details';
 })
 export class DetailsBonbonsComponent implements OnInit {
 
-  @Input() id: string;
+  	@Input() id: string;
 
-  constructor(private param_service: ApiDetailsService) { }
+	public myLocation: Location = new Location();
+	public coordonee = new Array;
+	
+ 	constructor(
+		  private param_service: ApiDetailsService,
+		  private locationService: LocationService
+		  ) { }
 
-  public details = new Details();
+  	public details = new Details();
 
   ngOnInit() {
+
+// 	Recuperation des information alimentaire de chaque bonbon
     return this.param_service.getDetails(this.id).subscribe(
       (data) => {
         this.details = data;
@@ -33,8 +44,17 @@ export class DetailsBonbonsComponent implements OnInit {
         this.details.keywords = data['product']['_keywords'];
 
       }
-    );
-
+	);
+// Recuperation des coordeonnée géographique
+//========================================================
+		this.coordonee = this.locationService.getMylocation()
+		console.log(this.coordonee);
+// Recuperation de l'Adress 
+//======================================================
+		this.locationService.getPositionInfo(1.011712, 48.463).subscribe(
+			(info: any) =>{
+			this.myLocation = info;
+		})
   }
 
 }
---
+++ src/app/models/location.ts
@@ -0,0 +1,7 @@
+export class Location {
+	city: 	 string;
+	street:  string;
+	region:   string;
+	adresse: string;
+	cityCode: string;
+}
